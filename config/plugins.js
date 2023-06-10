4fc25f7f21c483b10042ca87fde19b53e8ca3e14
Kevin Oliveira <me@kevinoliveira.com.br>
2023-06-09 23:15:08 -0300

    Config graphql plugin

--- config/plugins.js
+++ config/plugins.js
@@ -3,5 +3,18 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
   // ...
 });
