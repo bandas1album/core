a90a0946ee644afd7abaf790f1d3f41de792c55d
Kevin Oliveira <me@kevinoliveira.com.br>
2024-06-16 00:41:30 -0300

    Add database env

--- config/database.js
+++ config/database.js
@@ -1,15 +1,13 @@
-const path = require("path");
-
 module.exports = ({ env }) => ({
   connection: {
-    client: "sqlite",
+    client: "postgres",
     connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db")
-      ),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USER"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL"),
     },
-    useNullAsDefault: true,
   },
 });
