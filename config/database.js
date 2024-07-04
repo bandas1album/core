2b35e6603e966d433704076b9866fa6f302d98c8
Kevin Oliveira <me@kevinoliveira.com.br>
2024-07-04 10:00:52 -0300

    Update database production

--- config/database.js
+++ config/database.js
@@ -1,13 +1,15 @@
+const path = require("path");
+
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USER"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL"),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
+    useNullAsDefault: true,
   },
 });
